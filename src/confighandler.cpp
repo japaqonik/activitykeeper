b165c4d8cd580c55b3ffcd5458ed2f6d253161e8
japaqonik <japaqonik@gmail.com>
2021-05-07 08:42:20 +0200

    migrate to cxx 14

--- src/confighandler.cpp
+++ src/confighandler.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <cstring>
-#include <memory>
 #include "confighandler.h"
 
 ConfigHandler::ConfigHandler(IConfigWriter * _configWriter) : configWriter{_configWriter}
